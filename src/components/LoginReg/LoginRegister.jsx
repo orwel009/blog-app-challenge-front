28f4c2a6ae51139bc35ac97eaa65caac7808e32c
orwel009 <orwelvincent1@gmail.com>
2024-07-02 11:44:15 +0530

    partial commit

--- src/components/LoginReg/LoginRegister.jsx
+++ src/components/LoginReg/LoginRegister.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './LoginRegister.css'
+import { FaEnvelope, FaUser, FaLock } from "react-icons/fa";
 
 const LoginRegister = () => {
   return (
@@ -8,10 +9,12 @@ const LoginRegister = () => {
             <form action="">
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="text" placeholder='Username' required/>
+                    <input type="text" placeholder='Email' required/>
+                    <FaEnvelope className='icon'/>
                 </div>
                 <div className="input-box">
                     <input type="password" placeholder='Password' required/>
+                    <FaLock className='icon'/>
                 </div>
                 <div className="remember-forgot">
                     <label>
@@ -23,6 +26,34 @@ const LoginRegister = () => {
                 <button type='submit'>Login</button>
                 <div className="register-link">
                     <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+            </form>
+        </div>
+
+        <div className="form-box register">
+            <form action="">
+                <h1>Registration</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='Full Name' required/>
+                    <FaUser className='icon'/>
+                </div>
+                <div className="input-box">
+                    <input type="text" placeholder='Email' required/>
+                    <FaEnvelope className='icon'/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required/>
+                    <FaLock className='icon'/>
+                </div>
+                <div className="remember-forgot">
+                    <label>
+                        <input type="checkbox" />
+                        I agree to the terms & conditions
+                    </label>
+                </div>
+                <button type='submit'>Register</button>
+                <div className="register-link">
+                    <p>Already have an account? <a href="#">Login</a></p>
                 </div>
             </form>
         </div>
