b011844895d5acc81c145c4992473a74642a1a17
orwel009 <orwelvincent1@gmail.com>
2024-07-02 10:25:46 +0530

    initial commit

---
+++ src/components/LoginReg/LoginRegister.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './LoginRegister.css'
+
+const LoginRegister = () => {
+  return (
+    <div className='wrapper'>
+        <div className="form-box login">
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='Username' required/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required/>
+                </div>
+                <div className="remember-forgot">
+                    <label>
+                        <input type="checkbox" />
+                        Remember me
+                    </label>
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button type='submit'>Login</button>
+                <div className="register-link">
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default LoginRegister
